816b63ad29797164f177b51c27db88570ec7b9eb
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-08-25 01:14:13 +0100

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -8,7 +8,7 @@
 
 def print_last_digit(number):
     print(abs(number) % 10, end='')
-    return(abs(number) % 10)
+    return (abs(number) % 10)
 
 # #!/usr/bin/env python3
 # print_last_digit = __import__('9-print_last_digit').print_last_digit
