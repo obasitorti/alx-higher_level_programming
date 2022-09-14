3e60add8fd622e0c37842c9a0dfe02e93741e737
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-09-14 17:41:06 +0100

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = {}
-    for item in a_dictionary:
-        new_dictionary[item] = a_dictionary[item] * 2
-    return new_dictionary
+    return {x: j * 2 for x, j in a_dictionary.items()}
