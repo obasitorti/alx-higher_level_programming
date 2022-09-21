460d8870989068354b240410af39022a45fa3fea
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-09-21 13:42:57 +0100

    Create 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+    docstring for program
+"""
+
+
+class Square:
+    """
+        this class defines a square
+    """
+
+    def __init__(self, size=0):
+        """
+            arguments: self and size
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+            returns size of square
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+            sets the size of the square
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+            this method returns the area of a square
+        """
+        return self.__size ** 2
