0e5ac93e2d4ee148cd2f20f8817edcaa37c32aa7
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-09-21 12:38:50 +0100

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""
+    module documentation
+"""
+
+
+class Square(object):
+    """
+        this module defines a square class    
+    """
+
+    def __init__(self, size):
+        """
+            creating a private instance attribute size
+        """
+        self.size = size
