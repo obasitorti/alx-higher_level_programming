36c9fa477571fea15cdd74725bdf6a7d9bbe1335
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-09-21 14:54:49 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,12 +50,14 @@ class Square:
             determines the conditions for the co-ordinates 
             of the square
         """
-        if (type(value) is not tuple or
-            len(value) != 2 or value[0] is not int or value[1] is not int
-                or value[0] < 0 or value[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        if not isinstance(value, tuple):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+            
 
     def area(self):
         """
