0f4ab99c01fe0dfbb904a32e31e2ee73d09082bf
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-09-21 12:52:27 +0100

    Create 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    module to define a squuare class
+"""
+
+
+class Square:
+    """
+        definition of class square
+    """
+
+    def __init__(self, size=0):
+        """
+            arguments are self and size
+        """
+        self.__size = size
+        if size is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
