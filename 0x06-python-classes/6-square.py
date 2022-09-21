c379b4e96b7c0edea4f25e362abb3be899688a70
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-09-21 14:39:50 +0100

    Create 6-square.py

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+    project to define a class square
+"""
+
+
+class Square:
+    """
+        defines the class of the square
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+            initialization of the square
+            position is for the coordinates and
+            size is for the size
+        """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """
+            returns the size of the square
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+            determines the conditiones
+            for the size of the square
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """
+            returns the co-ordinates of the square
+        """
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """
+            determines the conditions for the co-ordinates 
+            of the square
+        """
+        if (type(value) is not tuple or
+            len(value) != 2 or value[0] is not int or value[1] is not int
+                or value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """
+            returns the area of the square
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """
+          prints the square using #s  
+        """
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * self.__size)
