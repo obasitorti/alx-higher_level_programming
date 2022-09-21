29e548f8737d9e23a33e1961d096b40f302957dc
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-09-21 14:47:47 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -67,6 +67,11 @@ class Square:
           prints the square using #s  
         """
         if self.__size == 0:
-            print()
+            print("")
+            return
+
+        [print("") for i in range(self.__position[1])]
         for i in range(self.__size):
-            print("#" * self.__size)
+            [print(" ", end="") for j in range(self.__position[0])]
+            [print("#", end="") for k in range(self.__size)]
+            print("")
