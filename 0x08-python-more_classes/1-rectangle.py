2d79eb95a8f6a0a4409abae4552cc0e8d6ff202d
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-09-28 17:29:26 +0100

    Create 1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Thsi module defines a class rectangle"""
+
+
+class Rectangle:
+    """This class defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Inittialisation of class recangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieves the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the condtions for the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieves the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the conditions for the height"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
