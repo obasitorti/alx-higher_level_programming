fcba54c11db0ba9531cbfedb1419bf5fd3487e08
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-09-30 00:34:19 +0100

    Create 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module defines a subclass MyList"""
+
+class MyList(list):
+    """prints a sorted list """
+    def print_sorted(self):
+        l = sorted(self)
+        print(l)
