ecb3760b06f206474c60e96af4c39f2540cc47e4
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-09-30 00:47:29 +0100

    Create 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this module defines a function"""
+
+
+def is_same_class(obj, a_class):
+    """returns if object is an instane of class"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
