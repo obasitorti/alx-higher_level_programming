323e95e9ee70ad2b45942e3f47f04f8d7a0ba48a
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-09-30 01:36:40 +0100

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """initialization of subclass"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
