4adb490a2e0826026836ed63520ff27c8c52ebdf
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-09-30 01:33:38 +0100

    Create 8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""module that defines empty class"""
+
+
+class BaseGeometry:
+    """defines two public attributes """
+
+    def area(self):
+        """defines area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """defines integer_validator"""
+        self.value = value
+        self.name = name
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+        defines a class rectangle inherited from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        """initialization of subclass"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
