e5f3442ccdf47b2c83049d632122439d54a66ba0
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-10-03 15:39:33 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@
 
 def read_file(filename=""):
     """function definition"""
-    with open(filename, encoding='utf-8') as a_file
+    with open(filename, encoding='utf-8') as a_file:
         print(a_file.read(), end="")
