ce34fe07c617799b2579f7d8dd859dd8f569cad0
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-10-03 16:37:51 +0100

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 
 def load_from_json_file(filename):
     """module defines creates object from json file"""
-    with open(filename, 'r', encoding='utf-8') as a_file:
+    with open(filename, mode='r', encoding='utf-8') as a_file:
         return json.load(a_file)
