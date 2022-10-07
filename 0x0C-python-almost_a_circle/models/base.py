a657f2e4070ac637e9c707be29c8297cae8ec7a9
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-10-07 21:39:13 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,4 +22,4 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return ("[]")
         else:
-            json.dumps(list_dictionaries)
+            return json.dumps(list_dictionaries)
