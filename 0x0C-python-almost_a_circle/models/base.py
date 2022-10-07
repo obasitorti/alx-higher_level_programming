f9b4fe86e8b3a703dacc9342f1bcab12f1edb71f
Obasi <104761686+obasitorti@users.noreply.github.com>
2022-10-07 22:05:20 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,7 +38,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """returns python list of a json string"""
-        if json_string is None or json_string == []:
+        if json_string is None or json_string == "":
             return ("[]")
         else:
             return json.loads(json_string)
